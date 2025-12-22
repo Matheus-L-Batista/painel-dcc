7adf963babedb2519b7faf9b402581a11234d657
Matheus Batista <matheuslbatita13@gmail.com>
2025-12-22 15:34:29 -0300

    Ajusta layout do painel

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Dash(
     __name__,
     use_pages=True,
     suppress_callback_exceptions=True,  # boa prática
+    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
 )
 server = app.server
 
