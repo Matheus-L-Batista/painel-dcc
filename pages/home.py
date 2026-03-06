96a1d2d67e8f40a17668dd5771187e4ec08ac56f
Matheus Batista <matheuslbatita13@gmail.com>
2026-03-06 10:04:17 -0300

    publicando arquivos do projeto

--- pages/home.py
+++ pages/home.py
@@ -4,15 +4,12 @@ from dash import html
 dash.register_page(
     __name__,
     path="/",
-    name="Início",
+    name="Home"
 )
 
 layout = html.Div(
     className="home-container",
     children=[
-        html.Img(
-            src="/assets/Painel DCC.png",
-            className="home-image"
-        )
+        html.Div(className="home-overlay")
     ]
 )
